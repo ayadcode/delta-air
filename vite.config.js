18a584bca6ed0ac882d08916947ebd61ab98d0a9
Mohamed Ayad <ayad@Haker.Mohamed>
2025-09-19 01:39:09 +0300

    applying some changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tagger from "@dhiwise/component-tagger";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "build",
+    chunkSizeWarningLimit: 2000,
+  },
+  plugins: [tsconfigPaths(), react(), tagger()],
+  server: {
+    port: "4028",
+    host: "0.0.0.0",
+    strictPort: true,
+  },
 });
